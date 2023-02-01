4020ef1e94ca3f4e93c6904c0800b02d2f1ec091
Mehran Alam Beigi <77109284+Mehranalam@users.noreply.github.com>
2023-02-01 16:23:34 +0330

    Create Program.cpp

---
+++ First_Euler_Problem/Program.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    int handle = 0;
+    while (handle < 1000){
+        if ((handle % 3 == 0) or (handle % 5 == 0)){
+            sum = sum + handle;
+        }
+        
+        handle++;
+    }
+    
+    std::cout << "Sum is : " << sum << std::endl;
+    return 0;
+}
+
+// Answer: 233168
